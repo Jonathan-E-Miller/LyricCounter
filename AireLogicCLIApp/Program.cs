71d15d914f0b6f1731febb2928fc7660f72eee6e
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-09-12 11:18:06 +0100

    Added NUnit Testing project
    Added a new NUnit testing project for running unit tests throughout development.

--- AireLogicCLIApp/Program.cs
+++ AireLogicCLIApp/Program.cs
@@ -2,7 +2,7 @@
 
 namespace AireLogicCLIApp
 {
-  class Program
+  public class Program
   {
     static void Main(string[] args)
     {
---
+++ AireLogicTests/Tests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+using AireLogicCLIApp;
+
+namespace AireLogicTests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
